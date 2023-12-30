a4ca7caf934c803410f970972fd69e81885b323a
André Silva <andr.sil142@gmail.com>
2023-12-30 12:31:19 -0300

    Correção de evento

--- main.js
+++ main.js
@@ -7,14 +7,17 @@ $(document).ready(function(){
         $('form').slideUp();
     })
 
-    $('form').on('submit', function(){
+    $('form').on('submit', function(e){
         e.preventDefault();
         const enderecoNovaImagem = $('#endereco-imagem-nova').val();
+        console.log(enderecoNovaImagem);
         const novoItem = $('<li style="display: none"></li>');
         $(`<img src="${enderecoNovaImagem}"/>`).appendTo(novoItem);
         $(`
             <div class="overlay-imagem-link>
-                <a href="${enderecoNovaImagem}" title="Ver imagem em tamanho real" target="_blank">Ver imagem em tamanho real</a>
+                <a href="${enderecoNovaImagem}" title="Ver imagem em tamanho real" target="_blank">
+                    Ver imagem em tamanho real
+                    </a>
             </div>
         `).appendTo(novoItem);
         $(novoItem).appendTo('ul');
